98860ff231befa5bb09baeda2bd53903fc89f80f
Utkarsh Gangwar <utkarshgangwar366@gmail.com>
2022-07-19 11:42:55 +0530

    trying to make billing app using xamarin forms

---
+++ BillingApp/AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BillingApp.ViewModels;
+using BillingApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BillingApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ BillingApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace BillingApp.ViewModels
+{
+    public class MainPageViewModel : ContentPage
+    {
+        public MainPageViewModel()
+        {
+            DizineUi();
+        }
+
+        public void DizineUi()
+        {
+            int year = Int32.Parse(DateTime.Now.Year.ToString());
+            int month = Int32.Parse(DateTime.Now.Month.ToString());
+            demo = month +" / "+year;
+        }
+        private string demo { get; set; }
+
+        public string Demo 
+        { 
+            get => demo; 
+            set
+            {
+                Demo = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
